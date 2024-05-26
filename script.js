c422374ff3ad756c51c0805e9f70ab707803e0fb
AriniGUIt <133998163+AriniGUIt@users.noreply.github.com>
2024-05-26 23:12:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const clickButton = document.getElementById("click-button");
-  const scoreDisplay = document.getElementById("score");
-  let score = 0;
-
-  clickButton.addEventListener("click", function() {
-    score++;
-    scoreDisplay.textContent = score;
-  });
-});
+/* CSS styles here */
